773b9e4b1093102f097790d22cae0cf66b5312a5
sneha sur <snehasur.myewards@gmail.com>
2024-08-06 09:49:48 +0530

    ofc

---
+++ react/myfirstreactapp/src/App.jsx
@@ -0,0 +1,27 @@
+import Hello from "./Hello";
+import Random from "./Random";
+import AppTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+import "./App.css";
+function App(){
+  // return <div>
+  //   <h1>this is the best react course</h1>
+  //   <Hello></Hello>
+  //   <Random></Random>
+  //   <Random></Random>
+  // </div>
+  //todo app
+  // return <center className="todo-container">
+  //   <AppName/>
+  //   <AppTodo/>
+  //   <div className="items-container">
+  //   <TodoItem1/>
+  //   <TodoItem2/>
+  //   </div>
+  // </center>
+  //
+}
+
+export default App;
