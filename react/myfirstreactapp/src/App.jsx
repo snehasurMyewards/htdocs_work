8eed8aca534e3e6503b1ffa19a42cbd615326699
sneha sur <snehasur.myewards@gmail.com>
2024-08-12 18:34:42 +0530

    test

--- react/myfirstreactapp/src/App.jsx
+++ react/myfirstreactapp/src/App.jsx
@@ -180,36 +180,44 @@ function App(){
 
 
 // }
-let [foodItems,setFoodItems]=useState([]);
-const onKeyDown=(event)=>{
-  if(event.key==='Enter'){
-    let newFoodItem=event.target.value;
-    event.target.value="";
-    console.log('Food item entered is:'+newFoodItem);
-    let newItems = [...foodItems,newFoodItem];
-    setFoodItems(newItems);
-  }
-}
-return (
-  <>
-  <Container>
-    <h1 className="food-heading">Healthy Food</h1>
-    <FoodInput handleKeyDown={onKeyDown}/>
-    <ErrorMessage items={foodItems}/>
-    <FoodItems items={foodItems}/>
-  </Container>
-  <Container> 
-    <p>
-      Above is the list of healthy food items.
-    </p>
+// let [foodItems,setFoodItems]=useState([]);
+// const onKeyDown=(event)=>{
+//   if(event.key==='Enter'){
+//     let newFoodItem=event.target.value;
+//     event.target.value="";
+//     console.log('Food item entered is:'+newFoodItem);
+//     let newItems = [...foodItems,newFoodItem];
+//     setFoodItems(newItems);
+//   }
+// }
+// return (
+//   <>
+//   <Container>
+//     <h1 className="food-heading">Healthy Food</h1>
+//     <FoodInput handleKeyDown={onKeyDown}/>
+//     <ErrorMessage items={foodItems}/>
+//     <FoodItems items={foodItems}/>
+//   </Container>
+//   <Container> 
+//     <p>
+//       Above is the list of healthy food items.
+//     </p>
 
      
-  </Container>
+//   </Container>
+//   </>
+  
+// )
+
+//calculator
+  return <>
+  <div className={style.calculator}> 
+    <Display/>
+    <ButtonsContainer/>
+  </div>
+
   </>
   
-)
-
-
 }
 
 
