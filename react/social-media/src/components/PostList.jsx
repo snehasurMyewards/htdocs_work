f4e3313c6de38a66d3b29118bbe50d5d1c5de120
sneha sur <snehasur.myewards@gmail.com>
2024-08-19 23:08:39 +0530

    test

--- react/social-media/src/components/PostList.jsx
+++ react/social-media/src/components/PostList.jsx
@@ -1,12 +1,16 @@
-import Post from "./Post"
-const PostList = () => {
-  return (
-    <>   
-      <Post/>
-      <Post/>
-      <Post/> 
-    </>
-  )
-}
+import { useContext } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
 
-export default PostList
+const PostList = () => {
+  const { postList } = useContext(PostListData);
+  return (
+    <>
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
+
+export default PostList;
