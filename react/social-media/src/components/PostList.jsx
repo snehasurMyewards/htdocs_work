0c1249a36fdb39611ab615addf01d74ee9e8a067
sneha sur <snehasur.myewards@gmail.com>
2024-08-21 17:03:52 +0530

    test

--- react/social-media/src/components/PostList.jsx
+++ react/social-media/src/components/PostList.jsx
@@ -1,11 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
-
+import WelcomeMessage from "./WelcomeMessage";
 const PostList = () => {
-  const { postList } = useContext(PostListData);
+  const { postList, addInitialPosts } = useContext(PostListData);
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+      });
+  }, []);
+  // const [dataFetched, setDataFetched] = useState(false);
+  // if (!dataFetched) {
+  //   fetch("https://dummyjson.com/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       addInitialPosts(data.posts);
+  //       setDataFetched(true);
+  //     });
+  // }
+  //const handelGetPostsClick = () => {
+  // console.log("Get Posts Clicked");
+  // fetch("https://dummyjson.com/posts")
+  //   .then((res) => res.json())
+  //   //.then(console.log);
+  //   .then((data) => {
+  //     addInitialPosts(data.posts);
+  //   });
+  //};
   return (
     <>
+      {postList.length === 0 && (
+        <WelcomeMessage
+        //onGetPostsClick={handelGetPostsClick}
+        />
+      )}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- react/social-media/src/store/post-list-store.jsx
+++ react/social-media/src/store/post-list-store.jsx
@@ -3,6 +3,7 @@ import { createContext, useReducer } from "react";
 export const PostList = createContext({
   postList: [],
   addPost: () => {},
+  addInitialPosts: () => {},
   deletePost: () => {},
 });
 
@@ -12,6 +13,8 @@ const postListReducer = (currPostList, action) => {
     newPostList = currPostList.filter(
       (post) => post.id !== action.payload.postId
     );
+  } else if (action.type === "ADD_INITIAL_POSTS") {
+    newPostList = action.payload.posts;
   } else if (action.type === "ADD_POST") {
     newPostList = [action.payload, ...currPostList];
   }
@@ -21,7 +24,8 @@ const postListReducer = (currPostList, action) => {
 const PostListProvider = ({ children }) => {
   const [postList, dispatchPostList] = useReducer(
     postListReducer,
-    DEFAULT_POST_LIST
+    //DEFAULT_POST_LIST
+    []
   );
 
   const addPost = (userId, postTitle, postBody, reactions, tags) => {
@@ -37,6 +41,14 @@ const PostListProvider = ({ children }) => {
       },
     });
   };
+  const addInitialPosts = (posts) => {
+    dispatchPostList({
+      type: "ADD_INITIAL_POSTS",
+      payload: {
+        posts,
+      },
+    });
+  };
 
   const deletePost = (postId) => {
     dispatchPostList({
@@ -48,29 +60,31 @@ const PostListProvider = ({ children }) => {
   };
 
   return (
-    <PostList.Provider value={{ postList, addPost, deletePost }}>
+    <PostList.Provider
+      value={{ postList, addPost, addInitialPosts, deletePost }}
+    >
       {children}
     </PostList.Provider>
   );
 };
 
-const DEFAULT_POST_LIST = [
-  {
-    id: "1",
-    title: "Going to Mumbai",
-    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
-    reactions: 2,
-    userId: "user-9",
-    tags: ["vacation", "Mumbai", "Enjoying"],
-  },
-  {
-    id: "2",
-    title: "Paas ho bhai",
-    body: "4 saal ki masti k baad bhi ho gaye hain paas. Hard to believe.",
-    reactions: 15,
-    userId: "user-12",
-    tags: ["Graduating", "Unbelievable"],
-  },
-];
+// const DEFAULT_POST_LIST = [
+//   {
+//     id: "1",
+//     title: "Going to Mumbai",
+//     body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
+//     reactions: 2,
+//     userId: "user-9",
+//     tags: ["vacation", "Mumbai", "Enjoying"],
+//   },
+//   {
+//     id: "2",
+//     title: "Paas ho bhai",
+//     body: "4 saal ki masti k baad bhi ho gaye hain paas. Hard to believe.",
+//     reactions: 15,
+//     userId: "user-12",
+//     tags: ["Graduating", "Unbelievable"],
+//   },
+// ];
 
 export default PostListProvider;
